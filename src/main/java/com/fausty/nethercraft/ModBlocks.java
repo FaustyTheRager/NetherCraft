31dd39d2b53e1c6b40b67484ca7d12c7a2f9c391
kosakriszi <kosakriszi@gmail.com>
2015-05-02 18:13:46 -0400

    fiery stone

--- src/main/java/com/fausty/nethercraft/ModBlocks.java
+++ src/main/java/com/fausty/nethercraft/ModBlocks.java
@@ -1,18 +1,19 @@
 package com.fausty.nethercraft;
 
 import com.fausty.nethercraft.block.BlockBase;
+import com.fausty.nethercraft.block.BlockFieryStone;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
 public class ModBlocks {
 
-    public static BlockBase testBlock;
-    public static BlockBase netherDirt;
-    public static BlockBase glowWood;
-    public static BlockBase heatSand;
-    public static BlockBase heatGlass;
-    public static BlockBase fieryStone;
-    public static BlockBase fieryCobblestone;
+    public static BlockBase       testBlock;
+    public static BlockBase       netherDirt;
+    public static BlockBase       glowWood;
+    public static BlockBase       heatSand;
+    public static BlockBase       heatGlass;
+    public static BlockFieryStone fieryStone;
+    public static BlockBase       fieryCobblestone;
 
     public static void create() {
         testBlock = new BlockBase("test_block", Material.rock);
@@ -20,7 +21,7 @@ public class ModBlocks {
         glowWood = new BlockBase("glow_wood", Material.rock, Block.soundTypeWood);
         heatSand = new BlockBase("heat_sand", Material.sand, Block.soundTypeSand);
         heatGlass = new BlockBase("heat_glass", Material.glass, Block.soundTypeGlass);
-        fieryStone = new BlockBase("fiery_stone", Material.rock, Block.soundTypeStone);
+        fieryStone = new BlockFieryStone();
         fieryCobblestone = new BlockBase("fiery_cobblestone", Material.rock, Block.soundTypeStone);
     }
 
---
+++ src/main/java/com/fausty/nethercraft/block/BlockFieryStone.java
@@ -0,0 +1,19 @@
+package com.fausty.nethercraft.block;
+
+import com.fausty.nethercraft.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+
+import java.util.Random;
+
+public class BlockFieryStone extends BlockBase {
+
+    public BlockFieryStone() {
+        super("fiery_stone", Material.rock, Block.soundTypeStone);
+    }
+
+    public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_) {
+        return Item.getItemFromBlock(ModBlocks.fieryCobblestone);
+    }
+}
