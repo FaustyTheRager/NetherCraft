9e3a4f38e5741c1b6f0ab898e8c1dce5fd49352d
kosakriszi <kosakriszi@gmail.com>
2015-05-02 17:23:47 -0400

    work on blockbase

--- src/main/java/com/fausty/nethercraft/ModBlocks.java
+++ src/main/java/com/fausty/nethercraft/ModBlocks.java
@@ -1,31 +1,19 @@
 package com.fausty.nethercraft;
 
 import com.fausty.nethercraft.block.BlockBase;
-import com.fausty.nethercraft.tab.Tabs;
-import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
 public class ModBlocks {
 
-    public static final BlockBase testBlock  = new BlockBase(Material.rock);
-    public static final BlockBase netherDirt = new BlockBase(Material.ground);
-    public static final BlockBase glowWood = new BlockBase(Material.wood);
+    public static BlockBase testBlock;
+    public static BlockBase netherDirt;
+    public static BlockBase glowWood;
 
     public static void create() {
-        testBlock.setBlockName("test_block");                   // Creation of testBlock
-        testBlock.setCreativeTab(Tabs.NETHERCRAFT);
-        GameRegistry.registerBlock(testBlock, "Test Block");
-
-        netherDirt.setBlockName("netherDirt");                  // Creation of netherDirt
-        netherDirt.setCreativeTab(Tabs.NETHERCRAFT);
-        GameRegistry.registerBlock(netherDirt, "Nether Dirt");
-
-
-        glowWood.setBlockName("glowWood");                   // Creation of glowWood
-        glowWood.setCreativeTab(Tabs.NETHERCRAFT);
-        glowWood.setStepSound(Block.soundTypeWood);
-        GameRegistry.registerBlock(glowWood, "Glow Wood");
+        testBlock = new BlockBase("test_block", Material.rock);
+        netherDirt = new BlockBase("nether_dirt", Material.ground);
+        glowWood = new BlockBase("glow_wood", Material.rock, Block.soundTypeWood);
     }
 
 }
--- src/main/java/com/fausty/nethercraft/block/BlockBase.java
+++ src/main/java/com/fausty/nethercraft/block/BlockBase.java
@@ -1,12 +1,25 @@
 package com.fausty.nethercraft.block;
 
+import com.fausty.nethercraft.tab.Tabs;
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
 public class BlockBase extends Block {
 
-    public BlockBase(Material material) {
+    public BlockBase(String internalName, Material material) {
         super(material);
+        this.setBlockName(internalName);
+        this.setCreativeTab(Tabs.NETHERCRAFT);
+        GameRegistry.registerBlock(this, internalName);
     }
 
+    public BlockBase(String internalName, Material material, SoundType sound) {
+        this(internalName, material);
+        this.setStepSound(sound);
+    }
+
+    public String getUnlocalizedName() {
+        return "nethercraft." + super.getUnlocalizedName().substring(5);
+    }
 }
