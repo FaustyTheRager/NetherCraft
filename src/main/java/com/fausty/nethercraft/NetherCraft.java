070d84aeee02cd2779bd2885cac808b06e4b7875
kosakriszi <kosakriszi@gmail.com>
2015-05-01 22:41:06 -0400

    fix directories for masta

--- src/main/java/com/fausty/nethercraft/NetherCraft.java
+++ src/main/java/com/fausty/nethercraft/NetherCraft.java
@@ -1,13 +1,13 @@
-package com.example.examplemod;
+package com.fausty.nethercraft;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod {
-    public static final String MODID = "examplemod";
+@Mod(modid = NetherCraft.MODID, version = NetherCraft.VERSION)
+public class NetherCraft {
+    public static final String MODID = "nethercraft";
     public static final String VERSION = "1.0";
     
     @EventHandler
