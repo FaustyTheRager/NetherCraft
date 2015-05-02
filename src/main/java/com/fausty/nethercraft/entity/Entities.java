ca5086844b2546fb2a659401e544c1c87cc1e573
kosakriszi <kosakriszi@gmail.com>
2015-05-02 12:16:42 -0400

    move rendering to proxy

--- src/main/java/com/fausty/nethercraft/entity/Entities.java
+++ src/main/java/com/fausty/nethercraft/entity/Entities.java
@@ -1,8 +1,6 @@
 package com.fausty.nethercraft.entity;
 
 import com.fausty.nethercraft.NetherCraft;
-import com.fausty.nethercraft.render.RenderLavaBoat;
-import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.registry.EntityRegistry;
 
 public class Entities {
@@ -10,7 +8,6 @@ public class Entities {
     public static void create() {
         EntityBoatBase.init();
         EntityRegistry.registerModEntity(EntityBoatBase.class, "LavaBoat", 1, NetherCraft.instance, 80, 3, true);
-        RenderingRegistry.registerEntityRenderingHandler(EntityBoatBase.class, new RenderLavaBoat());
     }
 
 }
--- src/main/java/com/fausty/nethercraft/proxy/ClientProxy.java
+++ src/main/java/com/fausty/nethercraft/proxy/ClientProxy.java
@@ -1,7 +1,13 @@
 package com.fausty.nethercraft.proxy;
 
+import com.fausty.nethercraft.entity.EntityBoatBase;
+import com.fausty.nethercraft.render.RenderLavaBoat;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+
 public class ClientProxy extends CommonProxy {
 
-
+    public ClientProxy() {
+        RenderingRegistry.registerEntityRenderingHandler(EntityBoatBase.class, new RenderLavaBoat());
+    }
 
 }
