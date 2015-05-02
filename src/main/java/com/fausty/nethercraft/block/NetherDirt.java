dcc8f4bcb3d31f1959d790086051beaa42b7a4b0
MastaC729 <salamence13@hotmail.com>
2015-05-01 23:10:48 -0400

    Create NetherDirt.java

---
+++ src/main/java/com/fausty/nethercraft/block/NetherDirt.java
@@ -0,0 +1,12 @@
+package com.fausty.nethercraft.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class NetherDirt extends Block {
+
+        public NetherDirt (Material material) {
+                super(material);
+        }
+
+}
