8253e7674b90c251edef8d4b9465ae88aa8e19c2
Nathan <faustytherager@gmail.com>
2015-05-02 18:11:48 -0400

    fiery stones

--- src/main/java/com/fausty/nethercraft/ModBlocks.java
+++ src/main/java/com/fausty/nethercraft/ModBlocks.java
@@ -9,11 +9,19 @@ public class ModBlocks {
     public static BlockBase testBlock;
     public static BlockBase netherDirt;
     public static BlockBase glowWood;
+    public static BlockBase heatSand;
+    public static BlockBase heatGlass;
+    public static BlockBase fieryStone;
+    public static BlockBase fieryCobblestone;
 
     public static void create() {
         testBlock = new BlockBase("test_block", Material.rock);
-        netherDirt = new BlockBase("nether_dirt", Material.ground);
+        netherDirt = new BlockBase("nether_dirt", Material.ground, Block.soundTypeGravel);
         glowWood = new BlockBase("glow_wood", Material.rock, Block.soundTypeWood);
+        heatSand = new BlockBase("heat_sand", Material.sand, Block.soundTypeSand);
+        heatGlass = new BlockBase("heat_glass", Material.glass, Block.soundTypeGlass);
+        fieryStone = new BlockBase("fiery_stone", Material.rock, Block.soundTypeStone);
+        fieryCobblestone = new BlockBase("fiery_cobblestone", Material.rock, Block.soundTypeStone);
     }
 
 }
