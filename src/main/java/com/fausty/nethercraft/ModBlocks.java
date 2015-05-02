2562fcf0aef477b634018c138476eab790bd5493
kosakriszi <kosakriszi@gmail.com>
2015-05-02 17:10:39 -0400

    sound fix

--- src/main/java/com/fausty/nethercraft/ModBlocks.java
+++ src/main/java/com/fausty/nethercraft/ModBlocks.java
@@ -3,6 +3,7 @@ package com.fausty.nethercraft;
 import com.fausty.nethercraft.block.BlockBase;
 import com.fausty.nethercraft.tab.Tabs;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
 public class ModBlocks {
@@ -10,7 +11,6 @@ public class ModBlocks {
     public static final BlockBase testBlock  = new BlockBase(Material.rock);
     public static final BlockBase netherDirt = new BlockBase(Material.ground);
     public static final BlockBase glowWood = new BlockBase(Material.wood);
-    public static final BlockBase.SoundType soundTypeWood = new BlockBase.SoundType("wood", 1.0F, 1.0F);
 
     public static void create() {
         testBlock.setBlockName("test_block");                   // Creation of testBlock
@@ -24,7 +24,7 @@ public class ModBlocks {
 
         glowWood.setBlockName("glowWood");                   // Creation of glowWood
         glowWood.setCreativeTab(Tabs.NETHERCRAFT);
-        glowWood.setStepSound(soundTypeWood);
+        glowWood.setStepSound(Block.soundTypeWood);
         GameRegistry.registerBlock(glowWood, "Glow Wood");
     }
 
