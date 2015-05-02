28dc17e7cc363543bbdbfe931b235738252c186e
Nathan <faustytherager@gmail.com>
2015-05-02 18:30:29 -0400

    - removed heat sand & glass - WIP soul glass

--- src/main/java/com/fausty/nethercraft/ModBlocks.java
+++ src/main/java/com/fausty/nethercraft/ModBlocks.java
@@ -10,17 +10,14 @@ public class ModBlocks {
     public static BlockBase       testBlock;
     public static BlockBase       netherDirt;
     public static BlockBase       glowWood;
-    public static BlockBase       heatSand;
-    public static BlockBase       heatGlass;
     public static BlockFieryStone fieryStone;
     public static BlockBase       fieryCobblestone;
+    //public static BlockSoulGlass  soulGlass;
 
     public static void create() {
-        testBlock = new BlockBase("test_block", Material.rock);
         netherDirt = new BlockBase("nether_dirt", Material.ground, Block.soundTypeGravel);
         glowWood = new BlockBase("glow_wood", Material.rock, Block.soundTypeWood);
-        heatSand = new BlockBase("heat_sand", Material.sand, Block.soundTypeSand);
-        heatGlass = new BlockBase("heat_glass", Material.glass, Block.soundTypeGlass);
+        //soulGlass = new BlockBase("soul_glass", Material.glass, Block.soundTypeGlass);
         fieryStone = new BlockFieryStone();
         fieryCobblestone = new BlockBase("fiery_cobblestone", Material.rock, Block.soundTypeStone);
     }
