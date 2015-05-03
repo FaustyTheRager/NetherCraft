67ab2e2f126a71a99c08796c0ea8a799eca6d72e
kosakriszi <kosakriszi@gmail.com>
2015-05-02 23:08:49 -0400

    paramaterized

--- src/main/java/com/fausty/nethercraft/block/BlockGlowBase.java
+++ src/main/java/com/fausty/nethercraft/block/BlockGlowBase.java
@@ -3,10 +3,10 @@ package com.fausty.nethercraft.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
-public class BlockGlowBase extends BlockBase{
+public class BlockGlowBase extends BlockBase {
 
-    public BlockGlowBase(){
-        super("glow_base",Material.wood, Block.soundTypeWood);
+    public BlockGlowBase(String internalName) {
+        super(internalName, Material.wood, Block.soundTypeWood);
         setLightLevel(1.0F);
     }
 
