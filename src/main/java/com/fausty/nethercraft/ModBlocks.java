835bcc5e95de654c67815de17fe9fe6030631f8c
Nathan <faustytherager@gmail.com>
2015-05-02 23:28:47 -0400

    - added Linium, Neridium, Pyridium & W ore - W ore has no texture

--- src/main/java/com/fausty/nethercraft/ModBlocks.java
+++ src/main/java/com/fausty/nethercraft/ModBlocks.java
@@ -14,10 +14,18 @@ public class ModBlocks {
     public static BlockFieryStone fieryStone;
     public static BlockBase       fieryCobblestone;
     public static BlockOreBase    fouliteOre;
+    public static BlockOreBase    liniumOre;
+    public static BlockOreBase    neridiumOre;
+    public static BlockOreBase    pyridiumOre;
+    public static BlockOreBase    wOre;
     //public static BlockSoulGlass  soulGlass;
 
     public static void create() {
+        wOre= new BlockOreBase("w_ore");
         fouliteOre = new BlockOreBase("foulite_ore");
+        liniumOre = new BlockOreBase("linium_ore");
+        neridiumOre = new BlockOreBase("neridium_ore");
+        pyridiumOre = new BlockOreBase("pyridium_ore");
         netherDirt = new BlockBase("nether_dirt", Material.ground, Block.soundTypeGravel);
         glowWood = new BlockGlowBase("glow_wood");
         //soulGlass = new BlockBase("soul_glass", Material.glass, Block.soundTypeGlass);
