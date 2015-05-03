077eecebde31f15c44b6d44e4c9cc94c90f0f214
Nathan <faustytherager@gmail.com>
2015-05-02 22:34:45 -0400

    - YOU CAN SEE NOW UR WELCUM

--- src/main/java/com/fausty/nethercraft/ModItems.java
+++ src/main/java/com/fausty/nethercraft/ModItems.java
@@ -6,7 +6,7 @@ import com.fausty.nethercraft.item.ItemLavaBoat;
 public class ModItems {
 
     public static ItemLavaBoat lavaBoat;
-    public static ItemBase     fouliteDust;
+    public static ItemBase fouliteDust;
 
     public static void create() {
         lavaBoat = new ItemLavaBoat("lava_boat");
