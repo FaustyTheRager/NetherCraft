8f45a92baf7c8745eadf9a1e785b4770e8b60687
faustytherager <faustytherager@gmail.com>
2015-05-12 07:40:09 -0400

    Left soul glass (needs to be class eventually maybe) Textured tear dust (probably modified later)

--- src/main/java/com/fausty/nethercraft/ModBlocks.java
+++ src/main/java/com/fausty/nethercraft/ModBlocks.java
@@ -9,18 +9,18 @@ import net.minecraft.block.material.Material;
 
 public class ModBlocks {
 
-    public static BlockBase            netherDirt;
-    public static BlockGlowBase        glowWood;
-    public static BlockGlowBase        glowWoodPlank;
-    public static BlockFieryStone      fieryStone;
-    public static BlockBase            fieryCobblestone;
+    public static BlockBase              netherDirt;
+    public static BlockGlowBase          glowWood;
+    public static BlockGlowBase          glowWoodPlank;
+    public static BlockFieryStone        fieryStone;
+    public static BlockBase              fieryCobblestone;
     public static BlockNetherOre         fouliteOre;
     public static BlockNetherOre         liniumOre;
     public static BlockNetherOre         neridiumOre;
     public static BlockNetherOre         pyridiumOre;
     public static BlockNetherOre         mourningOre;
-    public static BlockNetherWorkbench netherCraftingTable;
-    //public static BlockSoulGlass  soulGlass;
+    public static BlockNetherWorkbench   netherCraftingTable;
+    public static BlockBase              soulGlass;
 
     public static void create() {
         mourningOre = new BlockNetherOre("mourning_ore");
@@ -31,7 +31,7 @@ public class ModBlocks {
         netherDirt = new BlockBase("nether_dirt", Material.ground, Block.soundTypeGravel, 0.5F);
         glowWood = new BlockGlowBase("glow_wood", Material.wood);
         glowWoodPlank = new BlockGlowBase("glow_wood_plank", Material.wood);
-        //soulGlass = new BlockBase("soul_glass", Material.glass, Block.soundTypeGlass);
+        soulGlass = new BlockBase("soul_glass", Material.glass, Block.soundTypeGlass, 0.3F);
         fieryStone = new BlockFieryStone();
         fieryCobblestone = new BlockBase("fiery_cobblestone", Material.rock, Block.soundTypeStone, 2.0F);
         netherCraftingTable = new BlockNetherWorkbench();
